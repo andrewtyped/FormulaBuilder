7d636eb9721bd0934cf33b0579c0d264e9b13894
Andrew Barger <asbarg01@gmail.com>
2016-08-05 22:21:35 -0400

    roughing out a formula domain model

---
+++ FormulaBuilder.Core/Domain/Model/Formula.cs
@@ -0,0 +1,26 @@
+﻿using FormulaBuilder.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaBuilder.Core.Domain.Model
+{
+    public class Formula
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public Node RootNode { get; }
+
+        public Formula(FormulaEntity formulaEntity)
+        {
+            if (formulaEntity == null)
+                throw new ArgumentNullException(nameof(formulaEntity));
+
+            Id = formulaEntity.Id;
+            Name = formulaEntity.Name;
+            RootNode = new Node(formulaEntity.RootNode);
+        }
+    }
+}
---
+++ FormulaBuilder.Core/Models/Mappings/FormulaEntityMap.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaBuilder.Core.Models.Mappings
+{
+    public class FormulaEntityMap : ClassMap<FormulaEntity>
+    {
+        public FormulaEntityMap()
+        {
+            Schema("fb");
+            Table("Formula");
+            Id(x => x.Id).GeneratedBy.Identity();
+            Map(x => x.Name).Not.Nullable().Length(200);
+            References(x => x.RootNode).Cascade.None().Column("RootNodeId");
+        }
+    }
+}
